1b66e39d90465459f8936ecbf260cbac4780630a
Alex Crease <alex.crease@jdplc.com>
2023-12-13 00:39:03 +0000

    day 5 prep

---
+++ src/bin/5x1.rs
@@ -0,0 +1,13 @@
+use advent_23::read_lines;
+
+fn main() {
+    let lines = read_lines("data/day_05.txt");
+}
+
+#[cfg(test)]
+mod tests_5x1 {
+    #[test]
+    fn test() {
+        println!("hello world")
+    }
+}
